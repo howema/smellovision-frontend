3776f95591bf0b2a56051ddbf80caa5182fe4f53
MEG HOWE <howem9@gmail.com>
2021-10-05 14:50:55 -0400

    routes to show page by clicking on image

--- src/views/ShowsIndex.vue
+++ src/views/ShowsIndex.vue
@@ -3,9 +3,10 @@
     <h1>All Shows</h1>
     <div v-for="show in shows" v-bind:key="show.id">
       <h2>{{ show.name }}</h2>
-      <img v-bind:src="show.image" v-bind:alt="show.title" />
       <p></p>
-      <router-link v-bind:to="`/shows/${show.id}`">More details</router-link>
+      <router-link v-bind:to="`/shows/${show.id}`">
+        <img v-bind:src="show.image" v-bind:alt="show.title" />
+      </router-link>
       <p>Title: {{ show.title }}</p>
       <p>Year: {{ show.year }}</p>
       <p>Description: {{ show.description }}</p>
