d993e5f3948d3c39302c3b5596e0a7663c3000aa
MEG HOWE <howem9@gmail.com>
2021-10-07 15:00:36 -0400

    adds authentication

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@
       <router-link to="/shows">All shows</router-link>
       |
       <router-link to="/shows/new">New show</router-link>
+      |
+      <router-link to="/signup">Signup</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,9 @@ import ShowsIndex from "../views/ShowsIndex.vue";
 import ShowsNew from "../views/ShowsNew.vue";
 import ShowsShow from "../views/ShowsShow.vue";
 import ShowsEdit from "../views/ShowsEdit.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -26,6 +29,9 @@ const routes = [
   { path: "/shows/new", name: "shows-new", component: ShowsNew },
   { path: "/shows/:id", name: "shows-show", component: ShowsShow },
   { path: "/shows/:id/edit", name: "shows-edit", component: ShowsEdit },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 ];
 
 const router = new VueRouter({
